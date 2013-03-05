d093ce37f275f05e9b9d45cf784d5cde8aae8533
dpismenny <denis.pismenny@gmail.com>
2013-03-05 13:30:34 +0200

    map

--- js/common.js
+++ js/common.js
@@ -1,3 +1,14 @@
 $(document).ready(function() {
 
+	jQuery(".location li:has('ul')").toggle(
+		function(){
+			jQuery(this).children('ul').show();
+			return false;
+		},
+		function(){
+			jQuery(this).children('ul').hide();
+			return false;
+		}
+	);
+
 });
